ba33e0317b5d7785eb61f6d49b3737d5305fdd75
Владимир Любимов <emailoflvs@gmail.com>
2019-12-04 15:02:56 +0200

    Start project "bank-parser"

---
+++ app/Http/Controllers/Controller.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use http\Env\Request;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+use Illuminate\Foundation\Bus\DispatchesJobs;
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Routing\Controller as BaseController;
+
+
+class Controller extends BaseController
+{
+    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function index(Request $request)
+    {
+
+        return response()->json(['result' => 1]);
+    }
+
+
+}
